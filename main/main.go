d5e6d675a05f01dbc96e66ee3536c17f4ceca9c3
Daniel Schwartz <me@dansdomain.net>
2022-02-23 19:27:36 +0200

    completed exercise

--- main/main.go
+++ main/main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"os"
+
+	linkparser "dansdomain.net/html_link_parser"
 )
 
 func main() {
-	filename := flag.String("file", "ex1.html", "the html file to parse for links")
+	filename := flag.String("file", "ex4.html", "the html file to parse for links")
 	flag.Parse()
 	f, err := os.Open(*filename)
 	if err != nil {
 		log.Fatal("Error opening file", err)
 	}
 	defer f.Close()
+	
+	links, err := linkparser.Parse(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", links)
 }
--- parser.go
+++ parser.go
@@ -1,7 +1,65 @@
 package linkparser
 
-import "io"
+import (
+	"fmt"
+	"io"
+	"strings"
 
-func Parse(r *io.Reader) {
+	"golang.org/x/net/html"
+)
 
+func Parse(r io.Reader) ([]Link, error){
+	links := []Link{}
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	nodes := linkNodes(doc)
+	for _, node := range nodes {
+		fmt.Println(node)
+		links = append(links, buildLink(node))
+	}
+	return links, nil
+}
+
+func buildLink(n *html.Node) Link {
+	var ret Link
+	for _, attributes := range n.Attr {
+		if attributes.Key == "href" {
+			ret.Href = attributes.Val
+			break
+		}
+	}
+	ret.Text = text(n)
+	return ret
+}
+
+func text(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
+	var ret string
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret += text(c) + " "
+	}
+	return strings.Join(strings.Fields(ret), " ")
+}
+
+func linkNodes(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+	var ret []*html.Node
+	for c:= n.FirstChild; c!= nil; c = c.NextSibling {
+		ret = append(ret, linkNodes(c)...)
+	}
+	return ret
+}
+
+type Link struct {
+	Href string
+	Text string
 }
